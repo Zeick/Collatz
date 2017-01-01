cad852537051cb98f800403d7a29ba20ddfadadf
Urbanautti <manco888@gmail.com>
2017-01-01 19:17:54 +0200

    IGNORE THIS

--- Primus/src/main/java/Main.java
+++ Primus/src/main/java/Main.java
@@ -11,6 +11,6 @@
 public class Main {
 
     public static void main(String[] args) {
-System.out.println("Hello... I require a certain amount of content.");
+        System.out.println("Hello... I require a certain amount of content.");
     }
 }
